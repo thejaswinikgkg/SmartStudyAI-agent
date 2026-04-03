b8bd9cf19b7909569117bb679aaa38ba97542e8c
thejaswinikgkg <thejaswinikgkg@gmail.com>
2026-04-03 11:06:46 +0530

    Update Main.java
    interactive project

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Smart Study AI Agent");
 
-        int days = 3;
-        int subjects = 2;
+        System.out.print("Enter number of days left: ");
+        int days = sc.nextInt();
+
+        System.out.print("Enter number of subjects: ");
+        int subjects = sc.nextInt();
 
         for (int i = 1; i <= days; i++) {
             System.out.println("Day " + i + ": Study Subject " + ((i % subjects) + 1));
         }
+
+        sc.close();
     }
 }
